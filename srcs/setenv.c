123ff6d10de52a68e84d8768e936faa8e363c714
gmonein <mgaelbis@live.fr>
2017-11-10 15:10:03 +0100

    repair unsetenv et setenv

--- srcs/setenv.c
+++ srcs/setenv.c
@@ -1,44 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   setenv.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmonein <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/10 14:16:18 by gmonein           #+#    #+#             */
+/*   Updated: 2017/11/10 15:09:51 by gmonein          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
+
+static char		*get_node_name(char **args)
+{
+	char		*to_find;
+
+	if ((to_find = ft_strchr(args[0], '=')))
+		to_find = ft_strndup(args[0], (size_t)(to_find - args[0]));
+	else
+		to_find = ft_strdup(args[0]);
+	return (to_find);
+}
+
+static void		set_env_info(t_envnode *node, char **args)
+{
+	char		*buf;
+
+	ft_strdel(&node->info);
+	if ((buf = ft_strchr(args[0], '=')))
+		node->info = ft_strdup(buf + 1);
+	else if (args[1])
+		node->info = ft_strdup(args[1]);
+	else
+		node->info = NULL;
+}
 
 void	sh_setenv(t_list *envp, char **args)
 {
 	char		*to_find;
-	int			find;
-	t_list		*begin;
+	t_envnode	*node;
 	t_envnode	buf;
 
-	find = 0;
-	to_find = ft_strchr(args[0], '=');
-	if (to_find)
-		to_find = ft_strndup(args[0], (size_t)(to_find - args[0]));
-	else
-		to_find = ft_strdup(args[0]);
-	begin = envp;
-	while ((envp = envp->next))
-		if (!ft_strcmp(to_find, ((t_envnode *)envp->content)->name))
-		{
-			find = 1;
-			break;
-		}
-	if (find)
+	if (!args || !args[0])
+		return ;
+	to_find = get_node_name(args);
+	node = get_env_node(to_find, envp);
+	if (node)
 	{
 		ft_strdel(&to_find);
-		ft_strdel(&((t_envnode *)envp->content)->info);
-		if ((to_find = ft_strchr(args[0], '=')))
-			((t_envnode *)envp->content)->info = ft_strdup(to_find + 1);
-		else
-			((t_envnode *)envp->content)->info = ft_strdup(args[1]);
+		set_env_info(node, args);
 	}
 	else
 	{
-		while (begin->next)
-			begin = begin->next;
+		while (envp->next)
+			envp = envp->next;
 		buf.name = ft_strdup(to_find);
 		ft_strdel(&to_find);
-		if ((to_find = ft_strchr(args[0], '=')))
-			buf.info = ft_strdup(to_find + 1);
-		else
-			buf.info = ft_strdup(args[1]);
-		begin->next = ft_lstnew(&buf, sizeof(buf));
+		set_env_info(&buf, args);
+		envp->next = ft_lstnew(&buf, sizeof(buf));
 	}
 }
--- srcs/unsetenv.c
+++ srcs/unsetenv.c
@@ -4,25 +4,23 @@ void	sh_unsetenv(t_list *envp, char **args)
 {
 	t_list		*res;
 	t_list		*past;
-	int			find;
 
-	find = 0;
 	past = envp;
-	while ((envp = envp->next))
-	{
-		if (!ft_strcmp(args[0], ((t_envnode *)envp->content)->name))
+	if (args && args[0])
+		while ((envp = envp->next))
 		{
-			find = 1;
-			break;
+			if (envp->content)
+			{
+				if (!ft_strcmp(args[0], ((t_envnode *)envp->content)->name))
+				{
+					ft_strdel(&((t_envnode *)envp->content)->name);
+					ft_strdel(&((t_envnode *)envp->content)->info);
+					ft_memdel(&envp->content);
+					past->next = envp->next;
+					ft_memdel((void **)&envp);
+					return ;
+				}
+			}
+			past = past->next;
 		}
-		past = past->next;
-	}
-	if (find)
-	{
-		ft_strdel(&((t_envnode *)envp->content)->name);
-		ft_strdel(&((t_envnode *)envp->content)->info);
-		ft_memdel(&envp->content);
-		past->next = envp->next;
-		ft_memdel((void **)&envp);
-	}
 }
